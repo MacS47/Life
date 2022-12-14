78526b390af464c73777d2711e0f634f8eb020af
Alexsandre Silva <neto.machado22@gmail.com>
2022-12-14 13:59:06 -0300

    Add tela de introdução

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ screen_height = 580
 screen_playable_height = 480
 speed = 3
 speed_player = 3.1
-life = 100
+life = 10
+start_time = 0
 time_played = 0
 count_score = 0
 score = 0
@@ -14,6 +15,7 @@ level = 1
 map_loc = [[1,1],[2,1],[1,2],[2,2]]
 onetime = 0
 ia_increment = 60
+game_state = False
 
 # Random positioning
 def random_pos(initial=0):
@@ -54,7 +56,6 @@ def set_m_position_pixel(map_position):
         position[1] = random.randint(int(screen_playable_height/2),screen_playable_height)
     return position
 
-
 # Antibody movement
 def antibody_movement(antibody_rect):
     if (player_rect.x > antibody_rect.x):
@@ -76,7 +77,6 @@ def antibody_ia(antibody_rect,ia_increment):
     if distance < ia_increment:
          antibody_movement(antibody_rect)
 
-
 # Cell movement
 def cell_movement(cell_rect,dot_rect):
     speed_cell = speed
@@ -93,12 +93,13 @@ def cell_movement(cell_rect,dot_rect):
     if (dot_rect.y < cell_rect.y):
         cell_rect.y = cell_rect.y - speed_cell
 
-
 # Show info
 def display_info():
     # Info text
     player_life_text = font_text.render(f'LIFE: {life}',False, 'Black').convert_alpha()
     player_life_rect = player_life_text.get_rect(topleft = (0,game_name_text_rect.bottom+10))
+
+    time_played = int(pygame.time.get_ticks()/1000) - start_time
 
     player_time_played_text = font_text.render(f'    TIME: {time_played}',False, 'Black').convert_alpha()
     player_time_played_rect = player_time_played_text.get_rect(topleft = (player_life_rect.topright))
@@ -130,8 +131,9 @@ icon = pygame.image.load('images/Life_Logo.png')
 pygame.display.set_icon(icon)
 
 # Font of game
-font_title = pygame.font.Font('font/vga850.fon',24)
-font_text = pygame.font.Font('font/vga850.fon',18)
+font_title = pygame.font.Font('font/joystix monospace.ttf',18)
+font_text = pygame.font.Font('font/joystix monospace.ttf',12)
+font_game_over = pygame.font.Font('font/joystix monospace.ttf',24)
 
 # ----------------------------------------------
 
@@ -200,191 +202,215 @@ game_name_text_rect = game_name_text.get_rect(midtop = (screen_width/2,screen_pl
 
 while True:
 
-    # Screen color
-    screen.fill('White')
-    screen.blit(background,background_rect)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
 
-    # Useful technique for movements in only one vector
+    if game_state:
 
-    # if event.type == pygame.KEYDOWN:
-    #     if event.key == pygame.K_DOWN:
-    #         player_rect = player_rect.move(0,speed)
-    
-    # if event.type == pygame.KEYDOWN:
-    #     if event.key == pygame.K_UP:
-    #         player_rect = player_rect.move(0,speed*-1)
+        keys = pygame.key.get_pressed()
 
-    # if event.type == pygame.KEYDOWN:
-    #     if event.key == pygame.K_LEFT:
-    #         player_rect = player_rect.move(speed*-1,0)
-
-    # if event.type == pygame.KEYDOWN:
-    #     if event.key == pygame.K_RIGHT:
-    #         player_rect = player_rect.move(speed,0)
-
-    keys = pygame.key.get_pressed()
-
-    # Player movement
-    if keys[pygame.K_UP] and (keys[pygame.K_RIGHT] or keys[pygame.K_LEFT]):
-        player_rect = player_rect.move(0,speed_player*0.7071217222732374245*-1)
+        # Player movement
+        if keys[pygame.K_UP] and (keys[pygame.K_RIGHT] or keys[pygame.K_LEFT]):
+            player_rect = player_rect.move(0,speed_player*0.7071217222732374245*-1)
+        else:
+            if keys[pygame.K_UP]:
+                player_rect = player_rect.move(0,speed_player*-1)
+        
+        if keys[pygame.K_DOWN] and (keys[pygame.K_RIGHT] or keys[pygame.K_LEFT]):
+            player_rect = player_rect.move(0,speed_player*0.7071217222732374245)
+        else:
+            if keys[pygame.K_DOWN]:
+                player_rect = player_rect.move(0,speed_player)
+        
+        if keys[pygame.K_LEFT] and (keys[pygame.K_DOWN] or keys[pygame.K_UP]):
+            player_rect = player_rect.move(speed_player*0.7071217222732374245*-1,0)
+        else:
+            if keys[pygame.K_LEFT]:
+                player_rect = player_rect.move(speed_player*-1,0)
+        
+        if keys[pygame.K_RIGHT] and (keys[pygame.K_DOWN] or keys[pygame.K_UP]):
+            player_rect = player_rect.move(speed_player*0.7071217222732374245,0)
+        else:
+            if keys[pygame.K_RIGHT]:
+                player_rect = player_rect.move(speed_player,0)
     else:
-        if keys[pygame.K_UP]:
-            player_rect = player_rect.move(0,speed_player*-1)
-    
-    if keys[pygame.K_DOWN] and (keys[pygame.K_RIGHT] or keys[pygame.K_LEFT]):
-        player_rect = player_rect.move(0,speed_player*0.7071217222732374245)
-    else:
-        if keys[pygame.K_DOWN]:
-            player_rect = player_rect.move(0,speed_player)
-    
-    if keys[pygame.K_LEFT] and (keys[pygame.K_DOWN] or keys[pygame.K_UP]):
-        player_rect = player_rect.move(speed_player*0.7071217222732374245*-1,0)
-    else:
-        if keys[pygame.K_LEFT]:
-            player_rect = player_rect.move(speed_player*-1,0)
-    
-    if keys[pygame.K_RIGHT] and (keys[pygame.K_DOWN] or keys[pygame.K_UP]):
-        player_rect = player_rect.move(speed_player*0.7071217222732374245,0)
-    else:
-        if keys[pygame.K_RIGHT]:
-            player_rect = player_rect.move(speed_player,0)
+        keys = pygame.key.get_pressed()
 
-    player_path_current = set_map_position(player_rect)
+        if keys[pygame.K_SPACE]:
+            life = 10
+            start_time = int(pygame.time.get_ticks()/1000)
+            time_played = int(pygame.time.get_ticks()/1000) - start_time
+            count_score = 0
+            score = 0
+            level = 1
+            ia_increment = 60
+            game_state = True
 
-    if player_path_current != player_path[1]:
-        player_path[0] = player_path[1]
-        player_path[1] = player_path_current
 
-    free_path = player_path_current
-    aux = -1
+    if game_state:
+        
+        # Screen color
+        screen.fill('White')
+        screen.blit(background,background_rect)
 
-    while free_path == player_path[0] or free_path == player_path[1]:
-        aux = random.randint(0,3)
-        free_path = map_loc[aux]
+        player_path_current = set_map_position(player_rect)
 
-    # Render Player, Cell and antibody
-    screen.blit(player, player_rect)
-    screen.blit(cell, cell_rect)
-    screen.blit(antibody,antibody_rect)
-    screen.blit(antibody,antibody1_rect)
-    screen.blit(antibody,antibody2_rect)
-    screen.blit(antibody,antibody3_rect)
-    screen.blit(antibody,antibody4_rect)
-    screen.blit(antibody,antibody5_rect)
-    screen.blit(dot,dot_rect)
-    screen.blit(game_name_text,game_name_text_rect)
-    display_info()
+        if player_path_current != player_path[1]:
+            player_path[0] = player_path[1]
+            player_path[1] = player_path_current
 
-    if count_score == 10:
-        level += 1
-        count_score = 0
-        ia_increment += 10
+        free_path = player_path_current
+        aux = -1
+
+        while free_path == player_path[0] or free_path == player_path[1]:
+            aux = random.randint(0,3)
+            free_path = map_loc[aux]
+
+        # Render Player, Cell and antibody
+        screen.blit(player, player_rect)
+        screen.blit(cell, cell_rect)
+        screen.blit(antibody,antibody_rect)
+        screen.blit(antibody,antibody1_rect)
+        screen.blit(antibody,antibody2_rect)
+        screen.blit(antibody,antibody3_rect)
+        screen.blit(antibody,antibody4_rect)
+        screen.blit(antibody,antibody5_rect)
+        screen.blit(dot,dot_rect)
+        screen.blit(game_name_text,game_name_text_rect)
         display_info()
-    
 
-    # Collider Cell to Player
-    if cell_rect.colliderect(player_rect):
-        score += 1
-        count_score +=1
-        display_info()
-        cell_pos = random_pos(32)
-        cell_rect = cell.get_rect(center = cell_pos)
-    else:
+        if count_score == 10:
+            level += 1
+            count_score = 0
+            ia_increment += 10
+            display_info()        
+
+        # Collider Cell to Player
+        if cell_rect.colliderect(player_rect):
+            score += 1
+            count_score +=1
+            display_info()
+            cell_pos = random_pos(32)
+            cell_rect = cell.get_rect(center = cell_pos)
+        else:
+            # Calculate distance between Cell and Player
+            c_value = ((cell_rect.x-player_rect.x)**2)+((cell_rect.y-player_rect.y)**2)
+            distance_cell = math.sqrt(c_value)
+
+            # Calculate distance between Food and Player
+            c_value = ((dot_rect.x-player_rect.x)**2)+((dot_rect.y-player_rect.y)**2)
+            distance_dot = math.sqrt(c_value)
+
+            c_value = ((cell_rect.x-dot_rect.x)**2)+((cell_rect.y-dot_rect.y)**2)
+            distance_food_cell = math.sqrt(c_value)
+
+
+        # Movimentando a Célula
+        cell_movement(cell_rect,dot_rect)
+
+        # Verificando se o alimento e o player estão no mesmo bloco
+        # Se for verdadeiro o alimento será renderizado em um novo bloco
+        if set_map_position(dot_rect) == set_map_position(player_rect):
+
+            while free_path == player_path[0] or free_path == player_path[1]:
+                aux = random.randint(0,3)
+                free_path = map_loc[aux]
+
+            dot_pos = set_m_position_pixel(free_path)
+            dot_rect = dot.get_rect(center = dot_pos)
+            screen.blit(dot,dot_rect)
+
+        # Collider Cell to Food
+        if cell_rect.colliderect(dot_rect):
+            while free_path == player_path[0] or free_path == player_path[1]:
+                aux = random.randint(0,3)
+                free_path = map_loc[aux]
+            
+            dot_pos = set_m_position_pixel(free_path)
+            dot_rect = dot.get_rect(center = dot_pos)
+            screen.blit(dot,dot_rect)
+
+            # Trecho não utilizado
+            # # Calculate distance between Food and Player
+            # c_value = ((dot_rect.x-player_rect.x)**2)+((dot_rect.y-player_rect.y)**2)
+            # distance_dot = math.sqrt(c_value)
+        
         # Calculate distance between Cell and Player
         c_value = ((cell_rect.x-player_rect.x)**2)+((cell_rect.y-player_rect.y)**2)
         distance_cell = math.sqrt(c_value)
 
-        # Calculate distance between Food and Player
-        c_value = ((dot_rect.x-player_rect.x)**2)+((dot_rect.y-player_rect.y)**2)
-        distance_dot = math.sqrt(c_value)
-
-        c_value = ((cell_rect.x-dot_rect.x)**2)+((cell_rect.y-dot_rect.y)**2)
-        distance_food_cell = math.sqrt(c_value)
-
-
-    # Movimentando a Célula
-    cell_movement(cell_rect,dot_rect)
-
-    # Verificando se o alimento e o player estão no mesmo bloco
-    # Se for verdadeiro o alimento será renderizado em um novo bloco
-    if set_map_position(dot_rect) == set_map_position(player_rect):
-
-        while free_path == player_path[0] or free_path == player_path[1]:
-            aux = random.randint(0,3)
-            free_path = map_loc[aux]
-
-        dot_pos = set_m_position_pixel(free_path)
-        dot_rect = dot.get_rect(center = dot_pos)
-        screen.blit(dot,dot_rect)
-
-    # Collider Cell to Food
-    if cell_rect.colliderect(dot_rect):
-        while free_path == player_path[0] or free_path == player_path[1]:
-            aux = random.randint(0,3)
-            free_path = map_loc[aux]
         
-        dot_pos = set_m_position_pixel(free_path)
-        dot_rect = dot.get_rect(center = dot_pos)
-        screen.blit(dot,dot_rect)
+        # Antibody Collider
+        if antibody_rect.colliderect(player_rect):
+            life -= 1
+            display_info()
+            antibody_pos = random_pos()
+            antibody_rect = antibody.get_rect(center = antibody_pos)
+        
+        if antibody1_rect.colliderect(player_rect):
+            life -= 1
+            display_info()
+            antibody1_pos = random_pos()
+            antibody1_rect = antibody.get_rect(center = antibody1_pos)
+        
+        if antibody2_rect.colliderect(player_rect):
+            life -= 1
+            display_info()
+            antibody2_pos = random_pos()
+            antibody2_rect = antibody.get_rect(center = antibody2_pos)
+        
+        if antibody3_rect.colliderect(player_rect):
+            life -= 1
+            display_info()
+            antibody3_pos = random_pos()
+            antibody3_rect = antibody.get_rect(center = antibody3_pos)
+        
+        if antibody4_rect.colliderect(player_rect):
+            life -= 1
+            display_info()
+            antibody4_pos = random_pos()
+            antibody4_rect = antibody.get_rect(center = antibody4_pos)
+        
+        if antibody5_rect.colliderect(player_rect):
+            life -= 1
+            display_info()
+            antibody5_pos = random_pos()
+            antibody5_rect = antibody.get_rect(center = antibody5_pos)
+        else:
+            antibody_ia(antibody_rect,ia_increment)
+            antibody_ia(antibody1_rect,ia_increment)
+            antibody_ia(antibody2_rect,ia_increment)
+            antibody_ia(antibody3_rect,ia_increment)
+            antibody_ia(antibody4_rect,ia_increment)
+            antibody_ia(antibody5_rect,ia_increment)
 
-        # Trecho não utilizado
-        # # Calculate distance between Food and Player
-        # c_value = ((dot_rect.x-player_rect.x)**2)+((dot_rect.y-player_rect.y)**2)
-        # distance_dot = math.sqrt(c_value)
-    
-    # Calculate distance between Cell and Player
-    c_value = ((cell_rect.x-player_rect.x)**2)+((cell_rect.y-player_rect.y)**2)
-    distance_cell = math.sqrt(c_value)
-
-    
-    # Antibody Collider
-    if antibody_rect.colliderect(player_rect):
-        life -= 1
-        display_info()
-        antibody_pos = random_pos()
-        antibody_rect = antibody.get_rect(center = antibody_pos)
-    
-    if antibody1_rect.colliderect(player_rect):
-        life -= 1
-        display_info()
-        antibody1_pos = random_pos()
-        antibody1_rect = antibody.get_rect(center = antibody1_pos)
-    
-    if antibody2_rect.colliderect(player_rect):
-        life -= 1
-        display_info()
-        antibody2_pos = random_pos()
-        antibody2_rect = antibody.get_rect(center = antibody2_pos)
-    
-    if antibody3_rect.colliderect(player_rect):
-        life -= 1
-        display_info()
-        antibody3_pos = random_pos()
-        antibody3_rect = antibody.get_rect(center = antibody3_pos)
-    
-    if antibody4_rect.colliderect(player_rect):
-        life -= 1
-        display_info()
-        antibody4_pos = random_pos()
-        antibody4_rect = antibody.get_rect(center = antibody4_pos)
-    
-    if antibody5_rect.colliderect(player_rect):
-        life -= 1
-        display_info()
-        antibody5_pos = random_pos()
-        antibody5_rect = antibody.get_rect(center = antibody5_pos)
+        if life == 0:
+            game_state = False
+        
     else:
-        antibody_ia(antibody_rect,ia_increment)
-        antibody_ia(antibody1_rect,ia_increment)
-        antibody_ia(antibody2_rect,ia_increment)
-        antibody_ia(antibody3_rect,ia_increment)
-        antibody_ia(antibody4_rect,ia_increment)
-        antibody_ia(antibody5_rect,ia_increment)
+        # Screen color
+        screen.fill('White')
+        screen.blit(background,background_rect)
+
+        if life == 0:
+            
+            game_over_text = font_game_over.render(f'GAME OVER',False, 'Black').convert_alpha()
+            game_over_rect = game_over_text.get_rect(center = (screen_width/2,screen_playable_height/2))
+            screen.blit(game_over_text,game_over_rect)
+
+        else:
+            
+            game_over_text = font_game_over.render(f'LIFE - THE GAME',False, 'Black').convert_alpha()
+            game_over_rect = game_over_text.get_rect(center = (screen_width/2,screen_playable_height/2))
+            screen.blit(game_over_text,game_over_rect)
+
+        game_over_info_text = font_title.render(f'PRESS SPACE TO START',False, 'Black').convert_alpha()
+        game_over_info_rect = game_over_info_text.get_rect(center = (screen_width/2,(screen_playable_height/2)+30))
+        screen.blit(game_over_info_text,game_over_info_rect)
+
 
     # Refreshing the screen
     pygame.display.update()
